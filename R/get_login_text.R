6cf0d8c4b39df28c82d236c08b986e20c7945f8c
richelbilderbeek <richel@richelbilderbeek.nl>
2022-05-19 11:53:08 +0200

    Add 'get_login_text'

---
+++ R/get_login_text.R
@@ -0,0 +1,23 @@
+#' Get the Uppmax login text
+#' @return the Uppmax login text
+#' @examples
+#' cat(get_login_text())
+#' @author Richèl J.C. Bilderbeek
+#' @export
+get_login_text <- function() {
+  r"{
+  Last login: Wed May 18 13:19:00 2022 from bianca6.uppmax.uu.se
+   _   _ ____  ____  __  __    _    __  __
+  | | | |  _ \|  _ \|  \/  |  / \   \ \/ /   | System:    sens2021565-bianca
+  | | | | |_) | |_) | |\/| | / _ \   \  /    | User:      richel
+  | |_| |  __/|  __/| |  | |/ ___ \  /  \    |
+   \___/|_|   |_|   |_|  |_/_/   \_\/_/\_\   |
+
+  ###############################################################################
+
+  User Guides: http://www.uppmax.uu.se/support/user-guides
+  FAQ: http://www.uppmax.uu.se/support/faq
+
+  Write to support@uppmax.uu.se, if you have questions or comments.}"
+}
+
---
+++ tests/testthat/test-get_login_text.R
@@ -0,0 +1,3 @@
+test_that("use", {
+  expect_silent(get_login_text())
+})
